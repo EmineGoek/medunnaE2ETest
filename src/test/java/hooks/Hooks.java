02a7f81df1cccd26089c40533255aa921c7840af
Emine Gök <eminegok@gmx.de>
2023-06-06 23:08:22 +0200

    Initial commit

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,13 @@
+package hooks;
+
+import io.cucumber.java.Before;
+
+import static base_urls.MedunnaBaseUrl.setUp;
+
+public class Hooks {
+
+    @Before("@api")
+    public void beforeApi(){
+        setUp();
+    }
+}
